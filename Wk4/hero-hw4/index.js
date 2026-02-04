24cc5ffc46e64a726a6101950bec7c1791e710c1
lannicollene <an.picana@gmail.com>
2026-02-03 22:46:31 -1000

    added 2 additional npm module packages, created ReadMe file, added extension to code to return the 2 additional Node packages insatlled

--- Wk4/hero-hw4/index.js
+++ Wk4/hero-hw4/index.js
@@ -13,6 +13,23 @@ const Quote = require('inspirational-quotes');
 var myQuote = Quote.getRandomQuote();
 console.log(myQuote);
 
+// Allana's extension code for Week4a Assignment starts here: //
+// ============================= //
+
+// Create object and variable to display popular movie quotes
+const PopularMovieQuotes = require('popular-movie-quotes');
+var myMovieQuote = PopularMovieQuotes.getRandomQuote();
+console.log(myMovieQuote);
+
+// create object and variable to display famous last words
+const FamousLastWords = require('famous-last-words');
+var myFamousLastWords = FamousLastWords[0];
+console.log(myFamousLastWords);
+
+// ============================ //
+
+
+
 // creates a local web server and displays the above variables
 const http = require('http');
 const hostname = '127.0.0.1';
@@ -21,10 +38,30 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end("Super Hero: " + mySuperHeroName + "\nSuper Villain: " + mySuperVillainName + "\nQuote of the Day " + myQuote);
-    
+  
+  // Allana's comments: added the 2 additonal  variables to be displayed on the web server:
+  res.end(
+    "Super Hero: " + mySuperHeroName + 
+    "\nSuper Villain: " + mySuperVillainName + 
+    "\nQuote of the Day: " + myQuote + 
+    "\nPopular Movie Quote: " + myMovieQuote + 
+    "\nFamous Last Words: " + myFamousLastWords
+  );
+
+  // Allana's comments: added extension code to create 5 text files with the above variables:
+  const fileContents = 
+    "Super Hero: " + mySuperHeroName + 
+    "\nSuper Villain: " + mySuperVillainName + 
+    "\nQuote of the Day: " + myQuote+ 
+    "\nPopular Movie Quote: " + myMovieQuote + 
+    "\nFamous Last Words: " + myFamousLastWords;
+
     const fs = require("fs");
-  fs.writeFileSync("file2.txt", "Super Hero: " + mySuperHeroName + "\nSuper Villain: " + mySuperVillainName + "\nQuote of the Day " + myQuote);
+  fs.writeFileSync("File-1.txt", fileContents);
+  fs.writeFileSync("File-2.txt", fileContents);
+  fs.writeFileSync("File-3.txt", fileContents);
+  fs.writeFileSync("File-4.txt", fileContents);
+  fs.writeFileSync("File-5.txt", fileContents);
 
 });
 
---
+++ Wk4/hero-hw4/readMe.md
@@ -0,0 +1,24 @@
+# ICS 385 Spring 2026
+Week4a Assignment: Hero 
+
+# About this code:
+This extends original code to add Popular Movie Quote and Famous Last Words.
+
+The code displays the following, selected at random, and uisng five (5) different Node packages from NPM and then saved into five (5) separate text files:
+- Superhero
+- Villain
+- Inspirational Quote
+- Popular Movie Quote (New)
+- Famous Last Words (New)
+
+## File structure:
+hero-hw4/
+|--index.js
+|--package.json
+|--package-lock.json
+|--output/
+  |-- File-1.txt
+  |-- File-2.txt
+  |-- File-3.txt
+  |-- File-4.txt
+  |-- File-5.txt
