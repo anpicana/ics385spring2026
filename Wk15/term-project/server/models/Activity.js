37a7d5b1ae29455ef00c4644ce31c838d07f2f75
anpicana <anpicana@hawaii.edu>
2026-05-01 21:06:52 -1000

    updated code

--- Wk15/term-project/server/models/Activity.js
+++ Wk15/term-project/server/models/Activity.js
@@ -5,7 +5,11 @@ import mongoose from 'mongoose';
 const activitySchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    locationArea: { type: String, required: true, enum: ['Hilo', 'Kona', 'Waimea', 'Naalehu'] },
+    locationArea: { 
+      type: String, 
+      required: true, 
+      enum: ['Hilo', 'Kona', 'Waimea', 'Naalehu'] 
+    },
     description: { type: String, maxlength: 1000 },
     imageURL: String,
     bookingURL: String
